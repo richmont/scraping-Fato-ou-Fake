3b517ec7325312b047d1518fae355b25f1a79381
Richelmy <richelmy.monteiro@gmail.com>
2020-02-20 13:51:20 -0300

    Inserção agora funciona com id via kwargs, sendo opcional
    Caso o id não seja informado, o MongoDB cria um automaticamente
    função de requisição removida

--- banco.py
+++ banco.py
@@ -4,40 +4,40 @@ import requests
 url = "https://falkor-cda.bastian.globo.com/tenants/g1/instances/9a0574d8-bc61-4d35-9488-7733f754f881/posts/page/"
 
 
-def requisicao(url, pagina):
-    url = url + str(pagina)
-    resposta = requests.get(url)
-    json_data = json.loads(resposta.text)
-    tamanho_resposta = len(json_data["items"])
-    if tamanho_resposta <= 0:
-        return None
-    else:
-        return json_data
-
-
-def inserir(colecao, id, conteudo):
+def inserir(colecao, conteudo, **kwargs):
     """
     Parâmetros:
     colecao (MongoDB Collection)
-    id (int)
     conteudo (json)
+    id (str)
+    Em caso de ausência do id, programa continua e
+    deixa que o MongoDB gerencie o registro chave
 
     retorna o id do documento inserido
     """
-    resposta = colecao.find_one({"_id": id})
-    if resposta is None:
-        # elemento com id informado não existe no banco, inserir
-        # retorna o id inserido
-        campo = {}
-        id_campo = {"_id": id}
-        campo.update(id_campo)
-        campo.update(conteudo)
-        inserir = campo
-        resultado = colecao.insert_one(inserir)
-        return resultado.inserted_id
+
+    # se recebemos um id
+    id = kwargs.get("id")
+    if id is not None:
+        # verifique se este id já existe no banco
+        resposta = colecao.find_one({"_id": id})
+        if resposta is None:
+            # elemento com id informado não existe no banco, inserir
+            # retorna o id inserido
+            campo = {}
+            id_campo = {"_id": id}
+            campo.update(id_campo)
+            campo.update(conteudo)
+            inserir = campo
+            resultado = colecao.insert_one(inserir)
+            return resultado.inserted_id
+        else:
+            # elemento com id já existe no banco, retorna None
+            return None
     else:
-        # elemento com id já existe no banco, retorna None
-        return None
+        # se não recebemos uma id o Mongo cria automaticamente
+        resultado = colecao.insert_one(conteudo)
+        return resultado.inserted_id
 
 
 def alterar(colecao, busca, alterado):
@@ -70,20 +70,14 @@ dados = requisicao(url, 59)
 if dados is not None:
     print("dados válidos 59")
 
-"""
+
 # controle = {"_id": 1, "pagina_mais_antiga": 60}
 local = "mongodb://python:penis@localhost"
 cliente = MongoClient(local)
 banco = cliente.fato_ou_fake
 colecao = banco.dados_brutos
-# resultado = inserir(colecao, 1, {"ultima_pagina_verificada": 60})
-resultado = inserir(colecao, 12, {"pintos": 1212, "kkkk": True})
-if resultado is None:
-    print("erro")
-else:
-    print(resultado)
 
-"""
+
 busca = {"_id": 1}
 alterado = {"pagina_mais_antiga": 59}
 resultado = alterar(colecao, busca, alterado)
